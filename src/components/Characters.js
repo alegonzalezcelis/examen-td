8a1be68ec3ddf3882d1957ea1100e89db0fb7105
jorge valenzuela <jorge.valenzuela.diaz@ciisa.cl>
2023-04-18 20:48:40 +0000

    Update src/components/Characters.js

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -8,7 +8,7 @@ export default function Characters (props) {
     return (
     <div className="characters">
         <h1>Personajes</h1>
-        <span className="back-home" onClick={resetCharacters}>CAMBIO DE TEXTO NUEVAMENTE</span>
+        <span className="back-home" onClick={resetCharacters}>MODIFICACION PARA DESPLIEGUE</span>
         <div className="container-characters">
             {characters.map((character, index) => (
                 <div className="character-container" key={index}>
